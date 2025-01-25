318044b554c23fea9707a192c9e9067a50af1100
PAlagusurya <alagusuryap@gmail.com>
2025-01-25 20:21:56 +0530

    implemented GET POST methods

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
 require("dotenv").config();
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const data = req.body;
+  try {
+    await new User(data).save();
+    res.send("User created successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 connectDB()
   .then(() => {
@@ -11,4 +34,4 @@ connectDB()
       console.log("Listening on the port 3000");
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("Error connecting to DB", err));
