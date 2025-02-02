1a85f4a7cc902d455256eb50be092d9314bd1af7
PAlagusurya <alagusuryap@gmail.com>
2025-02-02 20:30:41 +0530

    Enhanced user schema with validation, default values, and additional fields

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,72 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 30,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error(`Enter a Strong Password: ${value}`);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    photoURL: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error(`Invalid Photo URL: ${value}`);
+        }
+      },
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "others"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    skills: {
+      type: [String],
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user!",
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
