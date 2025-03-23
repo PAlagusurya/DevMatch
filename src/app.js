23d3ea27601d7747d68c81fc1d2eb7976282b0d7
Alagusurya P <112260895+PAlagusurya@users.noreply.github.com>
2025-03-23 15:14:31 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,44 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+require("dotenv").config();
+
+app.use(
+  cors({
+    origin: "https://67dfd6cfc5172a73818ebbc0--developertinder.netlify.app",
+    credentials: true,
+  })
+);
+// Middleware to parse JSON data
+app.use(express.json());
+
+//Middlewarse to parse cookies
+app.use(cookieParser());
+
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
+
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
+
+connectDB()
+  .then(() => {
+    console.log("connected to DB successfully");
+    app.listen(3000, () => {
+      console.log("Listening on the port 3000");
+    });
+  })
+  .catch((err) => console.error("Error connecting to DB", err));",
     credentials: true,
   })
 );
